9d7709c27ee4748308663d3775292272400da897
Xa07 <srishreyanakshatri@gmail.com>
2023-07-11 23:48:00 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.body.style.background="yellow"
+// changes background color
+//document.getElementsByClassName("test")
+//dom manipulation and functions in javascript and cat page in html. link it in any photo 
