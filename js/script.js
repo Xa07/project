28a01bd8c7d9e46f276e702c208aa4930c1e4d81
Xa07 <srishreyanakshatri@gmail.com>
2023-07-26 22:04:09 +0530

    second commit

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.body.style.background="yellow"
-// changes background color
-//document.getElementsByClassName("test")
-//dom manipulation and functions in javascript and cat page in html. link it in any photo 
